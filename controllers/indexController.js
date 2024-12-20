7a0e95d938155a6fb48cba0ed9ee6e8f630f5db7
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2024-12-20 15:57:38 -0800

    progress on db search SQL

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,19 +1,24 @@
 const db = require("../db/queries");
 
 async function usernameGet(req, res) {
-  const usernames = await db.getAllUsernames();
-  console.log("Usernames: ", usernames);
-  res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
+  if (!(Object.keys(req.query).length === 0)) {
+    const { search } = req.query;
+    const matches = await db.searchUsername(search);
+    console.log(matches);
+    res.send("Search Result" + matches.map((user) => user.username).join(", "));
+  } else {
+    const usernames = await db.getAllUsernames();
+    res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
+  }
 }
 
-async function usernameCreateGet(req, res) {
+function usernameCreateGet(req, res) {
   res.render("createUsername", {
     title: "Create Username",
   });
 }
 
 async function usernameCreatePost(req, res) {
-  console.log(req.body);
   const { userName } = req.body;
   await db.insertUsername(userName);
   res.redirect("/");
--- db/queries.js
+++ db/queries.js
@@ -6,12 +6,20 @@ async function getAllUsernames() {
 }
 
 async function insertUsername(username) {
-  console.log(username);
-
   await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
+}
+
+async function searchUsername(query) {
+  const { rows } = await pool.query(
+    "SELECT * FROM usernames WHERE username LIKE ($1)",
+    [`%+${query}+%`]
+  );
+  // not working check SQL not matching
+  return rows;
 }
 
 module.exports = {
   getAllUsernames,
   insertUsername,
+  searchUsername,
 };
